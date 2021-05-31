0eed9fb1291742813a44702c33d1269b2c5d496f
David Rindt <davidrindt@gmail.com>
2021-05-31 16:00:20 +0100

    wrote covnet module

--- sumonet/model/net.py
+++ sumonet/model/net.py
@@ -21,15 +21,20 @@ totensor = ToTensor()
 
 
 def load_data(dataset):
+    """
+    :param dataset: str - name of the dataset
+    :return: - torch datasets - a train and test set
+    """
 
+    # Load the data
     if dataset == 'metabric':
         df = pycox.datasets.metabric.read_df()
     else:
         print('Dataset not found')
-
     cov, time, event = preprocess(df)
 
-    full_dataset = MyDataset(cov, time, event)
+    # Turn into a torch Dataset and split into train and test
+    full_dataset = SurvivalDataset(cov, time, event)
     train_size = int(np.floor(0.8 * len(full_dataset)))
     test_size = len(full_dataset) - train_size
     train_set, test_set = torch.utils.data.random_split(full_dataset, [train_size, test_size])
@@ -38,31 +43,45 @@ def load_data(dataset):
 
 
 def scale(arr, scaling_type):
+    """
+    :param arr: np.ndarry - to be scaled
+    :param scaling_type: - standard or minmax scaling
+    :return: ndarray - scaled array
+    """
 
+    # Select the scaler from sklearn
     assert scaling_type in ['standard', 'minmax']
-
     if scaling_type == 'standard':
         scaler = StandardScaler()
-
     else:
         scaler = MinMaxScaler()
 
-    df = scaler.fit_transform(arr)
+    # Do the scaling
+    arr = scaler.fit_transform(arr)
 
-    return df
+    return arr
+
 
 def preprocess(df, scaling_type_cov='standard', scaling_type_time='standard'):
+    """
+    :param df: pd.dataframe - to be preprocessed
+    :param scaling_type_cov: str - how to scale the covariates
+    :param scaling_type_time:  str - how to scale the times
+    :return: np.ndarray - cov, time, event
+    """
 
+    # Select the covariates, times, events
     cov = df.drop(['duration', 'event'], axis=1).to_numpy()
     time, event = df['duration'].to_numpy()[:, None], df['event'].to_numpy()[:, None]
 
+    # Perform the scaling
     cov = scale(cov, scaling_type_cov)
     time = scale(time, scaling_type_time)
 
     return cov, time, event
 
 
-class MyDataset(torch.utils.data.Dataset):
+class SurvivalDataset(torch.utils.data.Dataset):
     def __init__(self, cov, time, event):
         self.cov = torch.from_numpy(cov)
         self.time = torch.from_numpy(time)
@@ -75,23 +94,36 @@ class MyDataset(torch.utils.data.Dataset):
         return self.cov[idx], self.time[idx], self.event[idx]
 
 
-class Net(nn.Module):
+class CovNet(nn.Module):
     def __init__(self, config):
+        super().__init__()
         self.config = config
-        self.layers = self.config['layers']
-        self.hidden = nn.ModuleList()
-        self.activation = getattr(nn, config['activation'])()
-        for input_size, output_size in zip(self.layers, self.layers[1:]):
-            self.hidden.append(nn.Linear(input_size, output_size))
+        self.layer_widths = self.config['cov_layer_widths']
+        self.linear_transforms = nn.ModuleList()
+        self.activation = getattr(F, config['activation'])
+        for input_size, output_size in zip(self.layer_widths, self.layer_widths[1:]):
+            self.linear_transforms.append(nn.Linear(input_size, output_size))
 
 
     def forward(self, x):
-        return None
+        for linear_transform in self.linear_transforms:
+            x = self.activation(linear_transform(x))
+        return x
+
 
 
 if __name__ == '__main__':
 
+    # Initiate train, test set
     train_set, test_set = load_data('metabric')
-    print(len(train_set))
     print(len(test_set))
-    print(test_set[1])
+    cov, time, event = train_set[1:3]
+
+    # Define the config file
+    config = {'cov_layer_widths' : [9, 10, 1],
+              'activation': 'tanh'
+              }
+
+    # Initiate the net
+    net = CovNet(config)
+    print(net(cov))
